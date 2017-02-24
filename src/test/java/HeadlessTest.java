8f1ff241d86453ed82ff3d354c88778c243d96dd
Ben Williams <ben.williams@e-gineering.com>
2017-02-23 22:01:14 -0500

    added phantomjs

--- src/test/java/HeadlessTest.java
+++ src/test/java/HeadlessTest.java
@@ -1,18 +1,25 @@
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.phantomjs.phantomJSDriver;
-import io.github.bonigarcia.wdm.PhantomJSDriverManager;
+import org.openqa.selenium.TakesScreenshot;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import io.github.bonigarcia.wdm.PhantomJsDriverManager;
+import java.io.File;
 
-public class ExampleTest {
+public class HeadlessTest {
 
 	@Test
-	public void test(){
+	public void test() throws Exception{
 
-		PhantomJSDriverManager.getInstance().setup();
+		PhantomJsDriverManager.getInstance().version("1.9.8").setup();
 
-		WebDriver driver = new phantomJSDriver();
+		WebDriver driver = new PhantomJSDriver();
 
-    driver.get("http://www.amazon.com");
+    driver.get("http://www.google.com");
+
+    File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+    FileUtils.copyFile(scrFile, new File("/home/test/screenshot.png"));
 
 	}
 
